48e7318b57bcc9624501759469cb973171b3dcfc
DustinMcMullen <51675499+DustinMcMullen@users.noreply.github.com>
2020-08-24 21:41:35 -0500

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+ReactDOM.render(
+  <div>
+    <h1>Dustin's favorite things</h1>
+    <ul>
+      <li>Nikki</li>
+      <li>family</li>
+      <li>the fur-babies</li>
+    </ul>
+  </div>,
+  document.getElementById("root")
+);
